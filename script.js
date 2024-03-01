94447e39bb53c4d26467f06cbd10369fdca3076d
awproducoes <161882613+awproducoes@users.noreply.github.com>
2024-03-01 17:30:47 -0300

    script.js

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ document.getElementById("formCalculadora").addEventListener("submit", function(e
 
     // Obtém o valor do cachê inserido pelo usuário
     var cache = parseFloat(document.getElementById("cache").value);
+    var valorCobrado;
+
+    // Verifica se a opção de ISS está marcada
+    var iss = document.getElementById("iss").checked;
 
     // Calcula o valor a ser cobrado
-    var valorCobrado = cache / 0.79;
+    if (iss) {
+        valorCobrado = cache / (1 - (0.21 + 0.05));
+    } else {
+        valorCobrado = cache / 0.79;
+    }
 
     // Exibe o resultado na página
     document.getElementById("resultado").innerHTML = "Valor a ser cobrado: R$ " + valorCobrado.toFixed(2);
